a35f388b225e64cadf5983bc14b62d39fd961e99
Simonas <Simonas>
2023-02-19 17:32:09 +0200

    Add project files.

---
+++ kmp-api/Program.cs
@@ -0,0 +1,58 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    //options.AddPolicy("CorsAllowAll",
+    //    builder =>
+    //    {
+    //        builder
+    //        .AllowAnyOrigin()
+    //        .AllowAnyMethod()
+    //        .AllowAnyHeader()
+    //        .AllowCredentials();
+    //    });
+
+    //options.AddPolicy("CorsAllowSpecific",
+    //    p => p.WithHeaders("Content-Type", "Accept", "Auth-Token")
+    //        .WithMethods("POST", "PUT", "DELETE")
+    //        .SetPreflightMaxAge(new TimeSpan(1728000))
+    //        .AllowAnyOrigin()
+    //        .AllowCredentials()
+    //    );
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
