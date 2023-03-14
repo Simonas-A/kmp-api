b5270d4c304eb8f8d5b5a04a453f9eb10c5ec02f
Sostakas_Nojus <nojus.sostakas@ktu.edu>
2023-03-14 09:31:09 +0200

    Implemented image uploading to firebase

---
+++ kmp-api/Services/ImageService.cs
@@ -0,0 +1,36 @@
+﻿using Firebase.Auth;
+using Firebase.Storage;
+
+namespace kmp_api.Controllers
+{
+    public class ImageService
+    {
+        private string _firebaseStorageBucket = "BUCKET";
+
+        public Stream ConvertBase64ToStream(string imageFromRequest)
+        {
+            byte[] imageStringToBase64 = Convert.FromBase64String(imageFromRequest);
+            StreamContent streamContent = new(new MemoryStream(imageStringToBase64));
+            return streamContent.ReadAsStream();
+        }
+
+        public async Task<string> UploadImageToFirebase(Stream stream, string imageName)
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+            FirebaseStorageTask storageManager = new FirebaseStorage(
+                                _firebaseStorageBucket,
+                                new FirebaseStorageOptions
+                                {
+                                    ThrowOnCancel = true
+                                })
+                            .Child(imageName)
+                            .PutAsync(stream, cancellationToken.Token);
+
+            string imageFromFirebaseStorage;
+            imageFromFirebaseStorage = await storageManager;
+
+            return imageFromFirebaseStorage;
+        }
+    }
+}
